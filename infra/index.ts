52e6236a3f8826c3671a753b798fd7ffb8c379d6
Ido Shamun <idoesh1@gmail.com>
2020-11-04 11:32:05 +0200

    ci: add pulumi deployment

---
+++ infra/index.ts
@@ -0,0 +1,62 @@
+import * as gcp from '@pulumi/gcp';
+import {
+  addIAMRolesToServiceAccount,
+  createEnvVarsFromSecret,
+  infra,
+  location,
+} from './helpers';
+import { Output } from '@pulumi/pulumi';
+
+const name = 'redirector';
+
+const vpcConnector = infra.getOutput('serverlessVPC') as Output<
+  gcp.vpcaccess.Connector
+>;
+
+const serviceAccount = new gcp.serviceaccount.Account(`${name}-sa`, {
+  accountId: `daily-${name}`,
+  displayName: `daily-${name}`,
+}, {import: 'daily-redirector@devkit-prod.iam.gserviceaccount.com'});
+
+addIAMRolesToServiceAccount(
+  name,
+  [
+    { name: 'trace', role: 'roles/cloudtrace.agent' },
+    { name: 'secret', role: 'roles/secretmanager.secretAccessor' },
+    { name: 'pubsub', role: 'roles/pubsub.editor' },
+  ],
+  serviceAccount,
+);
+
+const secrets = createEnvVarsFromSecret(name);
+
+const service = new gcp.cloudrun.Service(name, {
+  name,
+  location,
+  template: {
+    metadata: {
+      annotations: {
+        'autoscaling.knative.dev/maxScale': '20',
+        'run.googleapis.com/vpc-access-connector': vpcConnector.name,
+      },
+    },
+    spec: {
+      serviceAccountName: serviceAccount.email,
+      containers: [
+        {
+          image:
+            `gcr.io/daily-ops/daily-${name}:ef26419069024f9214bdcad22daa966abe8e66c6`,
+          resources: { limits: { cpu: '1', memory: '256Mi' } },
+          envs: secrets,
+        },
+      ],
+    },
+  },
+});
+
+new gcp.cloudrun.IamMember(`${name}-public`, {
+  service: service.name,
+  location,
+  role: 'roles/run.invoker',
+  member: 'allUsers',
+});
